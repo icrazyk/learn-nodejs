a15489e92b719b63c9f7480850fc058c793596d0
dmitriy.karyakin <dmitriy.karyakin@walletone.com>
2017-01-13 14:15:40 +0500

    add templayer - lesson 32

--- kantor/30-chat/app.js
+++ kantor/30-chat/app.js
@@ -1,41 +1,43 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var config = require('config');
+var log = require('libs/log')(module);
+var ECT = require('ect');
+
+var ectRenderer = ECT({
+  watch: true,
+  root: __dirname + '/views',
+  ext: '.ect'
+});
 
 var app = express();
 
-app.set('port', 3000);
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.use(express.favicon());
 
-app.use(function(req, res, next)
+if(app.get('env') == 'development')
 {
-  if(req.url == '/') {
-    res.end("Hello");
-  } else {
-    next();
-  }
+  app.use(express.logger('dev'));
+}
+else
+{
+  app.use(express.logger('default'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.session({secret: 'qwerty'}));
+app.use(app.router);
+
+app.get('/', function(req, res, next)
+{
+  res.render('index', {});
 });
 
-app.use(function(req, res, next)
-{
-  if(req.url == '/forbidden') {
-    next(new Error("wops, denied"));
-  } else {
-    next();
-  }
-});
-
-app.use(function(req, res, next)
-{
-  if(req.url == '/test') {
-    res.end("Test");
-  } else {
-    next();
-  }
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(err, req, res, next)
 {
@@ -47,10 +49,12 @@ app.use(function(err, req, res, next)
   }
 });
 
-app.use(function(req, res, next)
-{
-  res.send(404, "Page Not Found");
-});
+http
+  .createServer(app)
+  .listen(config.get('port'), function()
+  {
+    log.info('Express server listening on port ' + config.get('port'));
+  });
 
 /*
 var routes = require('./routes');
@@ -59,14 +63,7 @@ var user = require('./routes/user');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.session({ secret: 'your secret here' }));
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
